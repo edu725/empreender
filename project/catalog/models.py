5f0d5fdeb517aaff57342bc87a326194f787aaf3
Eduardo Souza Ribeiro <160553234+edu725@users.noreply.github.com>
2024-07-30 21:00:15 -0300

    Update models.py

--- project/catalog/models.py
+++ project/catalog/models.py
@@ -6,9 +6,3 @@ class Item(models.Model):
     path = models.ImageField(upload_to="img/%Y/%m/%d/", blank=True)
     description = models.TextField()
 
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-
-    def __str__(self):
-        return f'{self.user} on {self.item}'
